7f02091b83790af78a72d06cf30a5ce94b1304ff
Stanislav <mail@skov.dev>
2021-10-10 00:19:57 +0300

    Updated WebSocket configuration

--- chat-online-service/src/main/java/local/chatonline/service/config/WebSocketConfig.java
+++ chat-online-service/src/main/java/local/chatonline/service/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package local.chatonline.service.config;
 
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -14,11 +13,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+        registry.addEndpoint("/chat");
+        registry.addEndpoint("/chat").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/chat-online");;
     }
 }
