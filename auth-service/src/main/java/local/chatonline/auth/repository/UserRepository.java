b146ac691b9457741d53e977ec25d2507aacd538
Stanislav <mail@skov.dev>
2021-10-27 12:48:18 +0300

    Added repository in auth-service

---
+++ auth-service/src/main/java/local/chatonline/auth/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package local.chatonline.auth.repository;
+
+import local.chatonline.auth.model.User;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
